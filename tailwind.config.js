f6c4079a3b1faada5e3ea37f1ef382f6b4aafbd4
jubaer.sookh <jubaer@sookh.com>
2023-05-05 17:59:42 +0600

    s

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px',
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+        roboto: "'Roboto', sans-serif",
+      },        
+    },
   },
   plugins: [],
 }
